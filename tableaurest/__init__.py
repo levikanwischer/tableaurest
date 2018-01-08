066a61aa9f3b44179477e2d181044a669a12277b
Levi Kanwischer <levi@kanwischer.me>
2018-01-07 17:25:00 -0800

    Bump version in develop to v0.4.dev

--- tableaurest/__init__.py
+++ tableaurest/__init__.py
@@ -7,7 +7,7 @@ import logging
 from tableaurest.core import TableauREST
 
 __title__ = 'tableaurest'
-__version__ = '0.3.0'
+__version__ = '0.4.0.dev'
 __license__ = 'MIT license'
 __author__ = 'Levi Kanwischer'
 __maintainer__ = 'Levi Kanwischer'
